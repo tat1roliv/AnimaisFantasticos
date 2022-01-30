4cf6a696f03fa9e51e8a600729dd7e982244baa5
tati oliveira <tat1roliv@gmail.com>
2022-01-30 16:56:26 -0300

    traversing-e-manipulacao.js

---
+++ assets/js/traversing-e-manipulacao.js
@@ -0,0 +1,41 @@
+/*
+outerHtml,
+innerHtml,
+innerText
+*/
+
+const menu = document.querySelector('.menu');
+
+console.log(menu.outerHTML); //todo html do elemento
+console.log(menu.innerHTML); //html interno
+console.log(menu.innerText); //texto, sem tags
+
+const h1 = document.querySelector('h1');
+
+//h1.innerText = 'titulo 1';//substitui conteudo
+//h1.innerHTML = '<p>Novo h1</p>'//acrescenta filho
+//h1.outerHTML = '<h2>Novo h1 trocando</h2>'//substitui
+
+
+
+/*
+transversing
+navegação pelo dom
+*/
+const lista = document.querySelector('.animais-lista');
+
+lista.parentElement; //pai
+lista.parentElement.parentElement; //pai do pai ate  o html
+lista.previousElementSibling; //elemento anterior
+lista.nextElementSibling; //elemento posterior
+
+lista.children; //HTMLCollection com filhos
+lista.children[0];//primeiro filho
+lista.children[lista.children.length-1];//ultimo filho
+
+lista.querySelectorAll('li');
+lista.querySelector('li:last-child');
+
+
+
+
