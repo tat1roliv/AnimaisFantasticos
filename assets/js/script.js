7825d3e90795782f65ce87ab1fd46c6ae5081377
tati oliveira <tat1roliv@gmail.com>
2022-02-01 20:26:46 -0300

    navegacao por tab

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/* navegacao por tab */
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if ( tabMenu.length && tabContent.length){ //if true && true
+
+    function activeTab(index){
+        tabContent.forEach( (section) => {
+            section.classList.remove('ativo');
+        })
+        tabContent[index].classList.add('ativo');
+    }
+    
+    tabMenu.forEach( (itemMenu, index) => {
+        itemMenu.addEventListener( 'click', () => {
+            activeTab(index);
+        });
+    })
+
+}
+
+
+
