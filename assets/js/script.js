16aa8252c6a6524a5aa0404bf2d3e7be8587fd3c
tati oliveira <tat1roliv@gmail.com>
2022-02-02 18:18:54 -0300

    accordion js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,59 @@
 /* navegacao por tab */
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav(){
 
-if ( tabMenu.length && tabContent.length){ //if true && true
-
-    function activeTab(index){
-        tabContent.forEach( (section) => {
-            section.classList.remove('ativo');
-        })
-        tabContent[index].classList.add('ativo');
-    }
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
     
-    tabMenu.forEach( (itemMenu, index) => {
-        itemMenu.addEventListener( 'click', () => {
-            activeTab(index);
-        });
-    })
+    if ( tabMenu.length && tabContent.length){ //if true && true
+    
+        function activeTab(index){
+            tabContent.forEach( (section) => {
+                section.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach( (itemMenu, index) => {
+            itemMenu.addEventListener( 'click', () => {
+                activeTab(index);
+            });
+        })
+    
+    }
+
 
 }
 
+initTabNav();
 
+
+
+
+/* accordion list */
+
+function initAccordion(){
+
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+
+    if (accordionList.length){
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion );
+        });
+
+    }
+
+
+}
+
+initAccordion();
 
